a90ed285260844378696bed4e895155feb69273e
BaptisteDCL <baptiste.duclos2@gmail.com>
2024-04-04 23:14:31 +0200

    Create Vowel Count.py

---
+++ Very_Easy/Vowel Count.py
@@ -0,0 +1,9 @@
+# Return the number (count) of vowels in the given string.
+
+# We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+# The input string will only consist of lower case letters and/or spaces.
+
+# Solution : 
+def get_count(sentence):
+    return sentence.count('a') + sentence.count('e') + sentence.count('i') + sentence.count('o') + sentence.count('u')
